22260ff7b968b0ddc67b3afbec7b5e5603ad9fb8
Alison MacFadyen <alison.macfadyen@tsl.ac.uk>
2024-10-02 12:13:50 +0100

    Update run_tracker.sh to add missing positional argument

--- scripts/run_tracker.sh
+++ scripts/run_tracker.sh
@@ -9,8 +9,9 @@ Rscript scripts/NLRtracker.R \
   $5 \
   $6 \
   $7 \
-  $8
+  $8 \
+  $9
 
 date > $5/done.txt
 #vars =
- # {params.ipro_list} {input.interpro} {input.fimo} {input.fa} {params.rundir} p {input.hmmer} {params.itol}
+ # {params.ipro_list} {input.interpro} {input.fimo} {input.fa} {params.rundir} {wildcards.sample} p {input.hmmer} {params.itol}
