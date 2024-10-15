8eaeaa8ca4e5034dc5ce185184c323433f6137b4
AlisonMacFadyen <alison.macfadyen86@gmail.com>
2024-10-15 09:51:12 +0100

    Update for interproscan and multi input

--- scripts/interpro.sh
+++ scripts/interpro.sh
@@ -5,6 +5,7 @@
 #source interproscan-5.45
 source nlrtracker-1.0.0
 
+
 dir=`pwd`
 fasta=$1
 tmpdir=$2
@@ -12,7 +13,7 @@ threads=$(($3 - 2))
 out=$4
 
 
-singularity exec --bind :$dir /tsl/software/testing/interproscan/5.69.101/x86_64/bin/interproscan_5.69-101.0.simg interproscan.sh -i $fasta \
+interproscan-5.70-102.0.sh -i $fasta \
   -f gff3 \
   -T $tmpdir \
   -cpu $threads \
